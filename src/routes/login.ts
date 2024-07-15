8ce22f48645ea798319aa85ec18f31fe19d59a4e
duyanh3110 <duy.nguyen@sarake.fi>
2024-07-15 15:07:17 +0300

    first commit

---
+++ src/routes/login.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const loginRouter = express.Router();
+
+loginRouter.get("/", (req, res) => {
+    res.send("This is the login request");
+})
+
+export default loginRouter;
